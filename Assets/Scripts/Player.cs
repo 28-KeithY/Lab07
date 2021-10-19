7bd3acd538ba7793b4043ff0b2a7474d240dd38d
KeithY14 <keithyong14@yahoo.com.sg>
2021-10-19 16:15:07 +0800

    Implementing jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float jumpforce;
+    public GameObject player;
+    private Rigidbody rb;
     private Animation thisAnimation;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            thisAnimation.Play();
+        if (player.transform.position.y < 3.69f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                thisAnimation.Play();
+                rb.velocity += Vector3.up * jumpforce;
+            }
+        }
+
     }
 }
